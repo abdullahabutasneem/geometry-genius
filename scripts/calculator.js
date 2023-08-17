cba415b15101fc15607f868d52bbecb0b917ff46
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-08-18 01:57:00 +0600

    getting the base and height value of triangle from input field

---
+++ scripts/calculator.js
@@ -0,0 +1,10 @@
+function calculateTriangleArea() {
+    // get triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+
+    const heightField = document.getElementById('triangle-height');
+    const heightValueText = heightField.value;
+    const height = parseFloat(heightValueText);
+}
