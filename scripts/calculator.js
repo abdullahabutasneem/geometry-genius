54551dd43ce872f3b288bd41ae937e925955cc03
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-08-18 02:16:21 +0600

    calculation done for Triangle

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -7,4 +7,12 @@ function calculateTriangleArea() {
     const heightField = document.getElementById('triangle-height');
     const heightValueText = heightField.value;
     const height = parseFloat(heightValueText);
+
+    const area = base * height;
+    const areaField = document.getElementById('triangle-area');
+    areaField.innerText = area;
+
+    const cmLine = document.getElementById('cm-line');
+    cmLine.classList.remove("hidden");
+
 }
